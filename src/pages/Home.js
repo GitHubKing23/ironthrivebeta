3bee98a35c856f6fc4bc2354726b805f42df1222
Jeff <wise11jeff@gmail.com>
2025-06-14 19:22:33 -0400

    Clean up and push changes for hero image and TailwindHero component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,19 @@
-// src/pages/Home.js
 import React from 'react';
-import HeroSection from '../components/HeroSection';
+import TailwindHero from '../components/TailwindHero';
 import AboutSection from '../components/AboutSection';
 import FeaturesSection from '../components/FeaturesSection';
 import TestimonialsSection from '../components/TestimonialsSection';
 import FinalCTA from '../components/FinalCTA';
+import heroImage from '../assets/hero.png'; // ✅ Correct image import
 
 const Home = () => {
   return (
     <div>
-      <HeroSection />
+      <TailwindHero
+        backgroundImage={heroImage}
+        location="Your City"
+        trainerName="Your Trainer"
+      />
       <AboutSection />
       <FeaturesSection />
       <TestimonialsSection />
